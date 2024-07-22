8aa8acb176f2a2e4cd33cf4e3ae3256d507df6b5
littlecm <clittle.norml@gmail.com>
2024-07-22 15:29:04 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 // pages/_app.js
-import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs';
+import '@/styles/globals.css';
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ClerkProvider>
-      <SignedIn>
-        <Component {...pageProps} />
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
+    <ClerkProvider {...pageProps}>
+      <header className="p-4 bg-gray-100 flex justify-between items-center">
+        <h1 className="text-xl font-bold">Dealership Forms</h1>
+        <div>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="bg-blue-500 text-white px-4 py-2 rounded">Sign In</button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </header>
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 }
